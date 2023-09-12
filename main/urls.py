a2c5fe61f3967e0f31f5db924a4d84ac0de2eda1
AousAlhajjaj <juan.d.khusuma@gmail.com>
2023-09-12 08:49:35 +0700

    9/12/2023

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from main.views import index
+from django.conf.urls.static import static
+from inventory_app.settings import STATIC_URL, STATIC_ROOT
 
 app_name = 'main'
 
 urlpatterns = [
     path('', index, name='index'),
-]
+] + static(STATIC_URL, document_root=STATIC_ROOT) 
